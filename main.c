2725f6195f4eaff9fa6f02d2cde9ea5183f1b199
Ubuntu <ubuntu@ip-10-240-205-171.us-west-2.compute.internal>
2024-06-05 08:12:57 +0000

    Fix segmentation fault issue by using fgets and removing newline

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     char name[64];
 
     printf("Enter your name:\n");
-    scanf("%s", name);
+    fgets(name, sizeof(name), stdin);
+
+    // Remove the trailing newline character if present
+    name[strcspn(name, "\n")] = '\0';
 
     printf("Hello %s\n", name);
 
